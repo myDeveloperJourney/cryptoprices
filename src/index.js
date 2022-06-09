a91b881f0eb8fb36093855b90cce758d837e4e05
Daniel JS <daniel@mydeveloperjourney.com>
2022-06-09 14:37:10 -0500

    wip: mounts react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+// Higher Order Component AKA HOC
+
+// higher order functions take functions as argument
+
+// Just like higher order function, they take components as arguments
+
+// named imports are designed to allow for multiple imports from a single statement
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* HOC pattern */}
+      <App /> 
+    </BrowserRouter>
   </React.StrictMode>
 );
 

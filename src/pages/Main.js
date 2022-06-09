4c17b7c1262e72767c38c8c3c855dec5e19a1491
Daniel JS <daniel@mydeveloperjourney.com>
2022-06-09 14:59:06 -0500

    wip: adds current pages

---
+++ src/pages/Main.js
@@ -0,0 +1,5 @@
+const Main = (props) => {
+    return <h1>This is the Main component</h1>;
+};
+
+export default Main;
---
+++ src/pages/Price.js
@@ -0,0 +1,5 @@
+const Price = (price) => {
+    return <h1>This is the Price component</h1>;
+};
+
+export default Price;
